8d7baae9d6d0dab7ba51b4dee866bc7eb8a30eba
js5323 <js5323@hotmail.com>
2018-06-14 12:13:40 +0800

    add dist to root task

---
+++ _tasks/plugins/distToRoot.js
@@ -0,0 +1,21 @@
+/**
+ * 将文件发布到根目录
+ */
+
+var util = require('./../lib/util');
+var path = require('path');
+var gulp = require("gulp");
+var del = require("del");
+var replace = require("gulp-replace");
+
+module.exports = function (config) {
+    console.log('dist to root');
+    gulp
+      .src("./dist/html/*.html")
+      .pipe(replace(/\.\.\//g, "./"))
+      .pipe(gulp.dest("./dist/"))
+      .on("end", function() {
+        del(["./dist/html/"]);
+      });
+
+}
